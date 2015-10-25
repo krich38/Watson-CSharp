abdd1adf3b680eeef88f2b738da5aefd7529817e
krich38 <krich38@hotmail.com>
2015-10-25 18:41:19 +1100

    Code Clean
    Cleaned and rewrote code

--- ConsoleApplication1/ConnectionWorker.cs
+++ ConsoleApplication1/ConnectionWorker.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
-using System.Collections.Specialized;
 using System.Security.Cryptography.X509Certificates;
 using System.Net.Security;
 using System.Net;
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net.Sockets;
-using System.IO;
-using System.Threading;
+﻿using System.Threading;
 
 namespace ConsoleApplication1
 {
